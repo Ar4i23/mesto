1b3b0c75a5ded7637c03838ab6549b7d2f19c8d4
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-03-10 15:24:26 +0300

    исправил js

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,14 +2,10 @@ document.getElementById("open-modal-btn").onclick = openModal;
 
 function openModal() {
   document.getElementById("my-modal").classList.add("open");
-  let infoTitle = document.querySelector(".profile__info-title").innerText;
-  let infoInputTitle = (document.querySelector(".modal__input_name").value =
-    infoTitle);
-  let infoSubtitle = document.querySelector(
-    ".profile__info-subtitle"
-  ).innerText;
-  let infoInputSubtitle = (document.querySelector(".modal__input_about").value =
-    infoSubtitle);
+  let nameInput = document.querySelector(".profile__info-title").innerHTML;
+  document.querySelector(".modal__input_name").value = nameInput;
+  let aboutInput = document.querySelector(".profile__info-subtitle").innerHTML;
+  document.querySelector(".modal__input_about").value = aboutInput;
 }
 
 document.getElementById("close-my-modal-btn").onclick = closeModal;
@@ -18,11 +14,11 @@ function closeModal() {
   document.getElementById("my-modal").classList.remove("open");
 }
 
-document.querySelector(".modal__button").onclick = myClick;
-
-function myClick() {
+let form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
   let imputName = document.querySelector(".modal__input_name").value;
   document.querySelector(".profile__info-title").innerHTML = imputName;
-  let imputAbout = document.querySelector(".modal__input_about").value;
-  document.querySelector(".profile__info-subtitle").innerHTML = imputAbout;
-}
+  let inputAbout = document.querySelector(".modal__input_about").value;
+  document.querySelector(".profile__info-subtitle").innerHTML = inputAbout;
+});
