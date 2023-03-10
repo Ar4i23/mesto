a009f24bb62da31d6b4b09da15e02d5ebbd715dd
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-03-10 04:16:30 +0300

    отредактировал большую часть

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,12 +3,12 @@ document.getElementById("open-modal-btn").onclick = openModal;
 function openModal() {
   document.getElementById("my-modal").classList.add("open");
   let infoTitle = document.querySelector(".profile__info-title").innerText;
-  let infoInputTitle = (document.querySelector(".modal__input-name").value =
+  let infoInputTitle = (document.querySelector(".modal__input_name").value =
     infoTitle);
   let infoSubtitle = document.querySelector(
     ".profile__info-subtitle"
   ).innerText;
-  let infoInputSubtitle = (document.querySelector(".modal__input-about").value =
+  let infoInputSubtitle = (document.querySelector(".modal__input_about").value =
     infoSubtitle);
 }
 
@@ -21,8 +21,8 @@ function closeModal() {
 document.querySelector(".modal__button").onclick = myClick;
 
 function myClick() {
-  let imputName = document.querySelector(".modal__input-name").value;
+  let imputName = document.querySelector(".modal__input_name").value;
   document.querySelector(".profile__info-title").innerHTML = imputName;
-  let imputAbout = document.querySelector(".modal__input-about").value;
+  let imputAbout = document.querySelector(".modal__input_about").value;
   document.querySelector(".profile__info-subtitle").innerHTML = imputAbout;
 }
