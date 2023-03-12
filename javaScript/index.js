e9885b888c23044ed4d2f9f6519d67e6a298340b
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-03-12 22:27:19 +0300

    переделал js

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,13 +1,18 @@
+const nameInputElement = document.querySelector(".modal__input_modal_name");
+const aboutInputElement = document.querySelector(".modal__input_modal_about");
+const titleElement = document.querySelector(".profile__info-title");
+const subtitleElement = document.querySelector(".profile__info-subtitle");
+console.log(nameInputElement.value);
+
 document.getElementById("open-modal-btn").onclick = openModal;
 
 function openModal() {
   document.getElementById("my-modal").classList.add("modal_open");
-  let nameInput = document.querySelector(".profile__info-title").textContent;
-  document.querySelector(".modal__input_modal_name").value = nameInput;
-  let aboutInput = document.querySelector(
-    ".profile__info-subtitle"
-  ).textContent;
-  document.querySelector(".modal__input_modal_about").value = aboutInput;
+
+  const inputName = titleElement.textContent;
+  nameInputElement.value = inputName;
+  const inputAbout = subtitleElement.textContent;
+  aboutInputElement.value = inputAbout;
 }
 
 document.getElementById("close-my-modal-btn").onclick = closeModal;
@@ -16,15 +21,11 @@ function closeModal() {
   document.getElementById("my-modal").classList.remove("modal_open");
 }
 
-let form = document.getElementById("form");
-form.addEventListener("submit", function (event) {
+document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
-  let imputName = document.querySelector(".modal__input_modal_name").value;
-  let newName = (document.querySelector(".profile__info-title").textContent =
-    imputName);
-  let inputAbout = document.querySelector(".modal__input_modal_about").value;
-  let newAbout = (document.querySelector(
-    ".profile__info-subtitle"
-  ).textContent = inputAbout);
+  const inputName = nameInputElement.value;
+  titleElement.textContent = inputName;
+  const inputAbout = aboutInputElement.value;
+  subtitleElement.textContent = inputAbout;
   closeModal();
 });
